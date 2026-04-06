263e8afd8d6ee407f150285d60cd2a791b331734
florianendres82 <florian.endres82@gmail.com>
2026-04-06 22:10:25 +0200

    Add vercel analystics and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter, Public_Sans } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/layout/Header"
 import Footer from "@/components/layout/Footer"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   variable: "--font-sans",
@@ -34,6 +36,8 @@ export default function RootLayout({
         <Header />
         <main className="flex-1">{children}</main>
         <Footer />
+        <Analytics />
+         <SpeedInsights />
       </body>
     </html>
   )
